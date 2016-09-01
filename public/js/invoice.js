f67706e58302c10541910ac84bc9a99b60200136
Mayank Sharma <mayanks@gmail.com>
2016-09-02 01:36:39 +0530

    read the order_id and user_id from the URL

--- public/js/invoice.js
+++ public/js/invoice.js
@@ -3,9 +3,19 @@ angular.module('myApp', ['ngSanitize'])
     function ($scope, $http, $filter) {
 
       $scope.orders = [];
+      var args = window.location.search.split('&');
+      var order_id = 'www-arbit';
+      var uid = '124';
 
-      var order_id = '2016-9-2';
-      var uid = '2857242207974196';
+      for (var i=0;i<args.length;i++) {
+        var name_value = args[i].split('=');
+        if (name_value[0].match(/uid/)) {
+         uid = name_value[1];
+        } else if (name_value[0].match(/order_id/)) {
+         order_id = name_value[1];
+        }
+      }
+
       $http.get('/data/orders/' + order_id + '/user.json?uid=' + uid).success(function (data) {
         $scope.orders = data;
       });
