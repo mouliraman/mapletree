78e802e6c8b5e1f882656b759e5dccfcb4462cce
Mayank Sharma <mayanks@gmail.com>
2016-12-25 15:53:33 +0530

    added a  new config file

---
+++ config.js
@@ -0,0 +1,23 @@
+const Fs = require('fs');
+
+var config = {};
+
+if (process.MODE == 'production') {
+  config.db_url = 'mongodb://localhost/mpt';
+  config.server_port = 3001;  
+  config.pg_api_key = '020705UANE81HLMY52AXY945741653K';
+  config.pg_merchant_id = 'A048';  
+  config.pg_private_key = Fs.readFileSync('fp.private.key');
+  config.pg_url = "https://secure.fonepaisa.com/pg/pay";
+  config.pg_callback_url = "https://mpt.revu.in/payment/";
+} else {
+  config.db_url = 'mongodb://localhost/mpt_dev';
+  config.server_port = 3000;  
+  config.pg_api_key = '08Z1782051U62BY9OUGW4XM67GF2004';
+  config.pg_merchant_id = 'FPTEST';
+  config.pg_private_key = Fs.readFileSync('fp.test.key');
+  config.pg_url = "https://test.fonepaisa.com/pg/pay";
+  config.pg_callback_url = "http://mpt.revu.in:3000/payment/";
+}
+
+module.exports = config;
